ac0df79bb0e0b8fe3013f10cf911389a10b952de
Trishix <trishitofficial@gmail.com>
2025-07-09 16:35:46 +0530

    mongo setup

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./db/index.js";
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express();
+// ( async ()=> {
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//         app.on("error", (error)=> {
+//             console.log("Error", error);
+//             throw error;
+//         })
+
+//         app.listen(process.env.PORT, ()=> {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.error("Error:", error);
+//         throw err
+//     }
+// })()
